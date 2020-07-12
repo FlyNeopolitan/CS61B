839312be8a94a75170c8fb7beef051db33da6645
jicheng <flyneopolitan@outlook.com>
2020-07-12 16:46:40 +0800

    update proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,14 +5,15 @@ import org.junit.Test;
 
 public class TestArrayDequeGold {
 
-    StudentArrayDeque<Integer> studentD = new StudentArrayDeque<Integer>();
-    ArrayDequeSolution<Integer> solutionD = new ArrayDequeSolution<Integer>();
+
     @Test
     public void randomTest() {
+        StudentArrayDeque<Integer> studentD = new StudentArrayDeque<Integer>();
+        ArrayDequeSolution<Integer> solutionD = new ArrayDequeSolution<Integer>();
         int cntOfAdd = 0;
         int cntOfRemove = 0;
         String operation = "";
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 50; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.25) {
                  operation = operation + "\naddFirst(" + i + ")";
@@ -28,14 +29,14 @@ public class TestArrayDequeGold {
 
             } else if (numberBetweenZeroAndOne < 0.75) {
                 if (cntOfAdd > cntOfRemove) {
-                    operation = operation + "\nremoveLast(" + i + ")";
+                    operation = operation + "\nremoveLast()";
                     assertEquals(operation, solutionD.removeLast(), studentD.removeLast());
                     cntOfRemove += 1;
 
                 }
             } else {
                 if (cntOfAdd > cntOfRemove) {
-                    operation = operation + "\nremoveFirst(" + i + ")";
+                    operation = operation + "\nremoveFirst()";
                     assertEquals(operation, solutionD.removeFirst(), studentD.removeFirst());
                     cntOfRemove += 1;
 
